436acba3f23ed7088412a7de0bb4713b7ca1dd55
Shanmugapriya Ravi <sravi@seattleu.edu>
2022-01-18 10:26:14 -0800

    Updated Default data in MockDataStore.cs

--- Mine/Mine/Services/MockDataStore.cs
+++ Mine/Mine/Services/MockDataStore.cs
@@ -14,12 +14,11 @@ namespace Mine.Services
         {
             items = new List<ItemModel>()
             {
-                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "First item", Description="This is an item description." },
-                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Second item", Description="This is an item description." },
-                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Third item", Description="This is an item description." },
-                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Fourth item", Description="This is an item description." },
-                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Fifth item", Description="This is an item description." },
-                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Sixth item", Description="This is an item description." }
+                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Golden Hand", Description="Sharp and Shining Sword" , Value=2 },
+                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Wind Fire", Description="Ice Flames and Fire over wind" , Value=5 },
+                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Thunder Bird", Description="Powerful Bird" , Value=3 },
+                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Sky Line", Description="Magic Blue Knife" , Value=4 },
+                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Dragon glass", Description="Glass knife" , Value=7 }
             };
         }
 
