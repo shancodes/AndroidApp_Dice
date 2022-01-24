32f36c0cde163cd90ed29b1aafd185455c48fdf0
Shanmugapriya Ravi <sravi@seattleu.edu>
2022-01-24 03:21:20 -0800

    Added ItemDeletePage to project

--- Mine/Mine/ViewModels/ItemIndexViewModel.cs
+++ Mine/Mine/ViewModels/ItemIndexViewModel.cs
@@ -41,6 +41,30 @@ namespace Mine.ViewModels
             return result;
         }
 
+        /// <summary>
+        /// Delete the record from the system
+        /// </summary>
+        /// <param name="data">The record to Delete</param>
+        /// <returns>True if Deleted</returns>
+        public async Task<bool> DeleteAsync(ItemModel data)
+        {
+            //Check if the record exists, if does not, then null is returned
+            var record = await ReadAsync(data.Id);
+            if (record == null)
+            {
+                return false;
+            }
+
+            //Remove from the local data set cache
+            DataSet.Remove(data);
+
+            //Call to remove it from the Data Store
+            var result = await DataStore.DeleteAsync(data.Id);
+
+            return result;
+
+        }
+
 
         async Task ExecuteLoadItemsCommand()
         {
