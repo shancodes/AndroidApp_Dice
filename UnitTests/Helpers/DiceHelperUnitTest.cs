8bea22658affdb0cfcdd585542b90b78719bf5b5
Shanmugapriya Ravi <sravi@seattleu.edu>
2022-02-08 10:41:35 -0800

    Added RollDice_Valid_Roll_2_Dice_6_Should_Return_Between_2_And_12  to DiceHelperUnitTest.cs

--- UnitTests/Helpers/DiceHelperUnitTest.cs
+++ UnitTests/Helpers/DiceHelperUnitTest.cs
@@ -53,6 +53,21 @@ namespace UnitTests.Helpers
             Assert.AreEqual(1, result);
         }
 
+        [Test]
+        public void RollDice_Valid_Roll_2_Dice_6_Should_Return_Between_2_And_12()
+        {
+            //Arrange
+
+            //Act
+            var result = DiceHelper.RollDice(2, 6);
+
+            //Reset
+
+            //Assert
+            Assert.AreEqual(true, result >= 2);
+            Assert.AreEqual(true, result <= 12);
+        }
+
     }
 }
 
